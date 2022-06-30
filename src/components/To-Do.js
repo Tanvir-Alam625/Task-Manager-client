43eb27fa65404de18e990844c05403b6719e1533
tanveer <mdtanviralamht731@gmail.com>
2022-06-30 19:29:40 +0600

    ToDo Component design done

--- src/components/To-Do.js
+++ src/components/To-Do.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 const ToDo = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const task = event.target.task.value;
+    console.log(task);
+  };
   return (
-    <div>
-      <h2>To Do</h2>
+    <div className="min-h-[400px] ">
+      <h2 className="text-center text-5xl my-6 font-extralight">Add Task </h2>
+      <div className="input-task-group flex justify-center">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="task"
+            placeholder="Type Your Task"
+            className="py-2 px-4 border-2 text-neutral outline-0 w-[400px] border-secondary rounded-l-md"
+          />
+          <button className="py-2  border-2 border-secondary px-4 bg-secondary rounded-r-md">
+            Add
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
